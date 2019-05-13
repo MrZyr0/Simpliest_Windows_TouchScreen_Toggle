8de1254af8363a4e9e2f3e73ba31533d4b02b01f
Julien <julien.seixas@free.fr>
2019-05-13 23:02:23 +0200

    V1
    + support of english US and french FR language system

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 Simpliest_Windows_TouchScreen_Toggle
+
+
+You just need to execute `script.ps1` in an administrator PowerShell ans magic happend !
+
+In the next version, the script can be run without administratif rights, in thé background ans with a simple keyboard shortcut.
---
+++ script.ps1
@@ -0,0 +1,17 @@
+﻿$lang = GET-WinSystemLocale | Select-Object -Expand Name
+
+switch ($lang)
+{
+    'fr-FR' { $device = '*Écran tactile HID*' }
+    'en-US' { $device = '*touch screen*' }
+}
+
+$status = Get-PnpDevice -FriendlyName $device | Select-Object  -Expand Status
+
+if ($status -eq 'OK'){
+    Get-PnpDevice -FriendlyName $device | Disable-PnpDevice -Confirm:$false
+}else{
+    Get-PnpDevice -FriendlyName $device | Enable-PnpDevice -Confirm:$false
+}
+
+Write-Host "Touch screen status changed"
